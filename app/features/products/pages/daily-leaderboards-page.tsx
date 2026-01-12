4b7b61d713d73c8ec98e9968e5f7b8fbd6c4448f
Natalie <natalie.nakyung.lee@gmail.com>
2026-01-12 20:27:57 +0900

    Implement initial setup for React Router configuration

---
+++ app/features/products/pages/daily-leaderboards-page.tsx
@@ -0,0 +1,75 @@
+import { DateTime } from "luxon";
+import type { Route } from "./+types/daily-leaderboards-page";
+import { data, isRouteErrorResponse } from "react-router";
+import { z } from "zod";
+import { PageHeader } from "~/common/components/page-header";
+
+const paramsSchema = z.object({
+  year: z.coerce.number(),
+  month: z.coerce.number(),
+  day: z.coerce.number(),
+});
+
+export const loader = ({ params }: Route.LoaderArgs) => {
+  const { success, data: parsedData } = paramsSchema.safeParse(params);
+  if (!success) {
+    throw data(
+      {
+        error_code: "invalid_params",
+        message: "Invalid params",
+      },
+      { status: 400 }
+    );
+  }
+  const date = DateTime.fromObject(parsedData).setZone("Asia/Seoul");
+  if (!date.isValid) {
+    throw data(
+      {
+        error_code: "invalid_date",
+        message: "Invalid date",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+  const today = DateTime.now().setZone("Asia/Seoul").startOf("day");
+  if (date > today) {
+    throw data(
+      {
+        error_code: "future_date",
+        message: "Future date",
+      },
+      { status: 400 }
+    );
+  }
+  return {
+    ...parsedData,
+    formattedDate: date.toFormat("yyyy/MM/dd"),
+  };
+};
+
+export default function DailyLeaderboardPage({
+    loaderData,
+  }: Route.ComponentProps) {
+    return (
+      <div className="container mx-auto px-4 py-8">
+        <PageHeader title={`The best of ${loaderData.formattedDate}`} />
+      </div>
+    );
+}
+
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        {error.data.message} / {error.data.error_code}
+      </div>
+    );
+  }
+  if (error instanceof Error) {
+    return <div>{error.message}</div>;
+  }
+  return <div>Unknown error</div>;
+}
